4683a91d3ba0984c36978ec2dd64b9f95ee62b09
chiarraeu <slavitransbg@gmail.com>
2024-12-07 18:48:19 +0200

    Initial commit

---
+++ resources/views/layouts/footers/auth/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="footer pb-4">
+    <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-6 mb-lg-0 mb-4">
+                <div class="copyright text-center text-sm text-muted text-lg-center">
+                    © {{ now()->year }} Developed by <a style="color: #252f40;" href="https://www.creative-tim.com" class="font-weight-bold ml-1"
+                        target="_blank">V Code Studio</a> for <a style="color: #252f40;" href="https://www.updivision.com"
+                        class="font-weight-bold ml-1" target="_blank">ELM LOGISTIK</a>
+                        <span class="text-bold">Nybro Sweden</span>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</footer>
